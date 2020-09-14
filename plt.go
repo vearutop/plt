f001ca10b7c7e9eaa173e8567544a361645ab0fc
Viacheslav Poturaev <vearutop@gmail.com>
2020-09-15 00:53:59 +0200

    Disable defaults for number and duration if any of those provided

--- plt.go
+++ plt.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/alecthomas/kingpin"
 	"github.com/vearutop/plt/curl"
 	"github.com/vearutop/plt/loadgen"
@@ -15,13 +17,13 @@ func main() {
 
 	lf := loadgen.Flags{}
 	kingpin.Flag("number", "Number of requests to run, 0 is infinite.").
-		Default("1000").IntVar(&lf.Number)
+		PlaceHolder("1000").IntVar(&lf.Number)
 	kingpin.Flag("concurrency", "Number of requests to run concurrently.").
 		Default("50").IntVar(&lf.Concurrency)
 	kingpin.Flag("rate-limit", "Rate limit, in requests per second, 0 disables limit (default).").
 		Default("0").IntVar(&lf.RateLimit)
 	kingpin.Flag("duration", "Max duration of load testing, 0 is infinite.").
-		Default("1m").DurationVar(&lf.Duration)
+		PlaceHolder("1m").DurationVar(&lf.Duration)
 	kingpin.Flag("slow", "Min duration of slow response.").
 		Default("1s").DurationVar(&lf.SlowResponse)
 	kingpin.Flag("live-ui", "Show live ui with statistics.").BoolVar(&lf.LiveUI)
@@ -29,4 +31,9 @@ func main() {
 	curl.AddCommand(&lf)
 
 	kingpin.Parse()
+
+	if lf.Number == 0 && lf.Duration == 0 {
+		lf.Number = 1000
+		lf.Duration = time.Minute
+	}
 }
