0b88246f36f84c3144f5c1ff506b1ee31863b0bc
Viacheslav Poturaev <vearutop@gmail.com>
2024-11-11 20:54:03 +0100

    Update deps (#39)

--- nethttp/http3.go
+++ nethttp/http3.go
@@ -13,7 +13,7 @@ import (
 const HTTP3Available = true
 
 func (j *JobProducer) makeTransport3() http.RoundTripper {
-	return &http3.RoundTripper{
+	return &http3.Transport{
 		TLSClientConfig: &tls.Config{
 			InsecureSkipVerify: true, //nolint:gosec // Allow insecure mode in a dev tool.
 		},
